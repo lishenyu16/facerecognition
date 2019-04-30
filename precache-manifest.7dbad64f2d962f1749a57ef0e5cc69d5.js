b4e3f7d6d24decd3b02d54e937eece691f156c56
Shenyu <lishenyu16@gmail.com>
2019-04-30 15:25:39 -0700

    Updates

---
+++ precache-manifest.7dbad64f2d962f1749a57ef0e5cc69d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c79a7b2c84aba2c383f5af72ffe4365",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "4fbe3e17e76e31759427",
+    "url": "/facerecognition/static/css/main.8115fd8d.chunk.css"
+  },
+  {
+    "revision": "0ea87ea320936f8e8105",
+    "url": "/facerecognition/static/js/2.7e0c37cf.chunk.js"
+  },
+  {
+    "revision": "4fbe3e17e76e31759427",
+    "url": "/facerecognition/static/js/main.ee6bd566.chunk.js"
+  },
+  {
+    "revision": "9a6a6a0c42958dd8e526",
+    "url": "/facerecognition/static/js/runtime~main.7f9ce1ea.js"
+  },
+  {
+    "revision": "b203c29319dfcc8f6070a09d48eeede5",
+    "url": "/facerecognition/static/media/icon.b203c293.png"
+  }
+]);
